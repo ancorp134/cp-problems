4394d7e012b75554cabd244acd29da4f9f868a08
Ankit Tyagi <ancorp007@gmail.com>
2023-01-27 10:58:25 +0530

    new file

---
+++ B_GCD_Partition.cpp
@@ -0,0 +1,48 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+ 
+ 
+void solve(){
+    ll n; cin>>n;
+    vl v(n);
+    ll sum=0;
+    fri(i,0,n){
+        cin>>v[i];
+        sum+=v[i];
+    }
+
+    ll ans=1 , tmp=0;
+    fri(i,0,n-1){
+        tmp+=v[i];
+        sum-=v[i];
+        ans = max(ans , __gcd(tmp,sum));
+    }
+    cout<< ans << endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
