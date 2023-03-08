0b6a178e3fc4bbce7f343443c997ad09b0e9a7a6
Ankit Tyagi <ancorp007@gmail.com>
2023-03-08 21:10:15 +0530

    new file

---
+++ A_Serval_and_Mocha_s_Array.cpp
@@ -0,0 +1,46 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+ 
+ 
+void solve(){
+    int n; cin>>n;
+    vi v(n);
+
+    fri(i,0,n) cin>>v[i];
+
+    int flag = 0;
+    fri(i,0,n){
+        fri(j,i+1,n){
+            if(__gcd(v[i],v[j])<=2) flag = 1;
+        }
+    }
+
+    cout<< (flag ? "Yes" : "No") << endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
