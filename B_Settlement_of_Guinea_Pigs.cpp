6fae17b19fd4537f6e859fdd53652270ccbc6a43
Ankit Tyagi <ancorp007@gmail.com>
2023-03-26 10:10:27 +0530

    new file

---
+++ B_Settlement_of_Guinea_Pigs.cpp
@@ -0,0 +1,46 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+ 
+ 
+void solve(){
+    int n; cin>>n;
+    int k = 0 , uk = 0 , ans=0;
+    fri(i,0,n){
+        int x; cin>>x;
+        if(x==1) uk++;
+        else {
+            k += uk;
+            uk=0;
+        }
+        ans = max(ans , uk + (k ? k/2 +1 : 0));
+    }
+
+    cout << ans << endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
