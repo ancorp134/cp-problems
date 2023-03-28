62e90a5e597b7e5b9911465bfa4302318c429792
Ankit Tyagi <ancorp007@gmail.com>
2023-03-28 22:45:00 +0530

    new file

---
+++ A_Walking_Master.cpp
@@ -0,0 +1,41 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+ 
+ 
+void solve(){
+    int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	if(b<=d&&c<=a+d-b){
+		cout<<(d-b)+(a+d-b-c)<<endl;
+        return;
+	}
+
+    cout<<-1<<endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
