acd072cf522666e46b5f3dcebe842d601c2cb27d
Ankit Tyagi <ancorp007@gmail.com>
2023-04-01 10:45:26 +0530

    new file

---
+++ A_Beautiful_Sequence.cpp
@@ -0,0 +1,43 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+ 
+ 
+void solve(){
+    int n; cin>>n;
+    int flag = 0;
+    fri(i,1,n+1){
+        int x; cin>>x;
+        if(x<=i){
+            flag = 1;
+        }
+    }
+
+    cout<< (flag?"YES":"NO")<<endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
