a667b230bed8a99f32ea183254b634de70461ea7
Ankit Tyagi <ancorp007@gmail.com>
2023-05-07 11:57:45 +0530

    new file

---
+++ A_Love_Story.cpp
@@ -0,0 +1,39 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+string t = "codeforces";
+ 
+void solve(){
+    string s; cin>>s;
+    int cnt=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=t[i]) cnt++;
+    }
+    cout<<cnt<<endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
