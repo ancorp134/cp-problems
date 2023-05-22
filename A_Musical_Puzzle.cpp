bba2ab9706763bd6e3f55ea2805fd30bb40b8ee5
Ankit Tyagi <ancorp007@gmail.com>
2023-05-22 17:45:39 +0530

    new file

---
+++ A_Musical_Puzzle.cpp
@@ -0,0 +1,48 @@
+// Author : Ankit Tyagi
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fri(i,a,b) for(int i=a;i<b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define vi vector<int>
+#define vl vector<ll>
+#define pp pop_back
+#define all(x) (x).begin(), (x).end()
+#define dec greater<int>
+#define endl "\n"
+ 
+ 
+ 
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+
+    set<string> se;
+
+    for(int i=0;i<n-1;i++){
+        string t = "";
+        t += s[i];
+        t += s[i+1];
+        if(!se.count(t)){
+            se.insert(t);
+        }
+    }
+
+    cout<<se.size()<<endl;
+    return;
+}
+     
+
+ 
+int main(){
+	fast;
+	int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
